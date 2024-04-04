12b2d18d3b86701f29a7dd3f8f0a3a16135f7170
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-04-04 21:21:34 +0100

    Added wakelock

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
   import GameControls from './lib/components/GameControls.svelte';
   import Hand from './lib/components/Hand.svelte'
+
+  async function requestWakeLock() {
+    try {
+      const wakeLock = await navigator.wakeLock.request('screen');
+      console.log('Screen wake lock is active');
+
+      // Listen for visibility change or page hide events to release and re-acquire the wake lock
+      document.addEventListener('visibilitychange', async () => {
+        if (document.visibilityState === 'visible' && wakeLock.released) {
+          await navigator.wakeLock.request('screen');
+          console.log('Screen wake lock is re-acquired');
+        }
+      });
+
+      return wakeLock;
+    } catch (err) {
+      console.error(`Could not acquire wake lock: ${err}`);
+    }
+  }
+
+  // Call this function when your app initializes, or when it's appropriate to acquire the wake lock
+  requestWakeLock();
 </script>
 
 <main>
