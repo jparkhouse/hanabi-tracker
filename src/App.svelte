9bccfe7257fe2b1f6efaa29c3ec090d29352f9ef
Jake <jparkhouse1511@gmail.com>
2024-04-26 19:24:09 +0100

    build: added version number to bottom text

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
   import GameControls from './lib/components/GameControls.svelte';
   import Hand from './lib/components/Hand.svelte'
+  import { version } from '../package.json';
 </script>
 
 <main>
   <GameControls />
   <Hand />
-  <a href="https://github.com/jparkhouse/hanabi-tracker/" target="_blank"><u>Check out my code!</u></a>
+  <div class="bottom-text">
+    version {version} ·
+    <a href="https://github.com/jparkhouse/hanabi-tracker/" target="_blank"><u>Check out my code!</u></a>
+  </div>
 </main>
+
+<style>
+  .bottom-text {
+    display: flex;
+    flex-direction: left;
+    align-content: center;
+    justify-content: center;
+    gap: 4px;
+  }
+</style>
