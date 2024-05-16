f7cade93df45a6ec73e5a0f9dfc9e4dd5068310a
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-05-16 20:19:55 +0100

    build: added testing env

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hanabi-tracker/',
+  base: process.env.VITE_BASE_URL,
   plugins: [svelte()],
 })
