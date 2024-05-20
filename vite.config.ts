5e6dead49586c575a9575232401b2bbf2a9cac97
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-05-20 21:37:00 +0100

    build: setting up testing env

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL,
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [svelte()],
 })
