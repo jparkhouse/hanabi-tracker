a3a5bec52a2c86dd1cccc44d82d80de898de4410
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-05-22 18:30:41 +0100

    build: passing env to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
-  plugins: [svelte()],
-})
+export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...require(`./.env.${mode}`)};
+  return {
+    base: mode === 'release' ? '/hanabi-tracker' : mode === 'testing' ? '/hanabi-tracker/test' : '/',
+    plugins: [svelte()],
+  };
+});
