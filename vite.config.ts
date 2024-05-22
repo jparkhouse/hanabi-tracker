c043e7f478da22b51084d586da5ab974ec935ced
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-05-22 18:40:23 +0100

    build: added dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
+import * as dotenv from 'dotenv';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}`});
+
 export default defineConfig(({ mode }) => {
   process.env = {...process.env, ...require(`./.env.${mode}`)};
   return {
